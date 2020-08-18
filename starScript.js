24b0a7fdfdc31bf727f1d77bd33f644d8934b395
ReubeniSandwich <Reubenisanchez@gmail.com>
2020-08-17 20:15:07 -0400

    updated website to use js to handle the VH problem v3

--- starScript.js
+++ starScript.js
@@ -3,12 +3,12 @@ let screenHeight = window.innerHeight;
 
 homepage.style.setProperty("height", `${screenHeight}px`);
 
-function updateScreenSize () {
-    screenHeight = window.innerHeight;
-    homepage.style.setProperty("height", `${screenHeight}px`);
-    console.log(screenHeight);
-}
-window.addEventListener("resize", updateScreenSize);
+// function updateScreenSize () {
+//     screenHeight = window.innerHeight;
+//     homepage.style.setProperty("height", `${screenHeight}px`);
+//     console.log(screenHeight);
+// }
+// window.addEventListener("resize", updateScreenSize);
 
 
 
