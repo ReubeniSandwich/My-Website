9aefad326175d0022dd6b89bd4f323c1f7c48aa5
ReubeniSandwich <Reubenisanchez@gmail.com>
2020-08-21 13:10:00 -0400

    somewhat stable treeline

--- starScript.js
+++ starScript.js
@@ -1,7 +1,27 @@
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
+let height = window.innerHeight;
+let width = window.innerWidth;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+document.documentElement.style.setProperty
+
+trees = document.querySelector('.svg-tree-shape');
+// console.log(shape);
+
+trees.setAttribute("style", `width:${width}px`);
+
+
+// window.addEventListener('resize', updateTreeSize);
+
+// function updateTreeSize () {
+//     width = window.innerWidth;
+//     console.log("hellpo");
+//     trees.setAttribute("style", `width:${width}px`);
+
+// }
+
 
 
 
