7eebbe10f135ae1099d46a460cce7d65074d1fcf
ReubeniSandwich <Reubenisanchez@gmail.com>
2020-08-25 14:50:57 -0400

    addded sticky navigation. adjusted different tree heights for variety. added test stuff

--- starScript.js
+++ starScript.js
@@ -1,32 +1,35 @@
+const trees = document.querySelector('.svg-tree-shape');
+const navigation = document.querySelector('.header-blackbox.navbar') 
+let topOfNav = navigation.offsetTop;
 let width = document.documentElement.clientWidth;
 let height = document.documentElement.clientHeight;
 
-
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = document.documentElement.clientHeight * 0.01;
-
-
-console.log({height, width});
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-document.documentElement.style.setProperty
+//Not working at all
+// function updateTreeSize () {
+//     width = document.documentElement.clientWidth;
+//     trees.setAttribute("style", `width:${width}px`);
+// }
 
-trees = document.querySelector('.svg-tree-shape');
-// console.log(shape);
+function updateNavigation () {
+    if (window.scrollY >= topOfNav) {
+        // document.body.style.paddingTop = navigation.offsetHeight + 'px';
+        navigation.classList.add("sticky-navigation");
+    } else {
+        navigation.classList.remove("sticky-navigation");
+        document.body.style.paddingTop = 0;
+    }
+}
 
 trees.setAttribute("style", `width:${width}px`);
 
-
-// window.addEventListener('resize', updateTreeSize);
-
-// function updateTreeSize () {
-//     width = window.innerWidth;
-//     console.log("hellpo");
-//     trees.setAttribute("style", `width:${width}px`);
-
-// }
-
+// not working atm
+// document.addEventListener('resize', updateTreeSize);
+window.addEventListener('scroll', updateNavigation);
 
 
 
