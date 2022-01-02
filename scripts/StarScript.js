840e5485d13d1b99cad29daf02ef4975cc7f89a6
Reuben Sanchez <reubenisanchez@gmail.com>
2022-01-02 10:24:09 -0500

    cleanup star code and update scrolldown arrow

--- scripts/StarScript.js
+++ scripts/StarScript.js
@@ -1,11 +1,7 @@
-const trees = document.querySelector('.svg-tree-shape');
 let width = document.documentElement.clientWidth;
 let height = document.documentElement.clientHeight;
 
-console.log(width);
-console.log(height);
-
-function generateRandomStars(maxWidth, maxHeight, amount) {
+function generateRandomStarLocations(maxWidth, maxHeight, amount) {
   let starList = [];
   
   for (let i = 0; i < amount; i++) {
@@ -21,29 +17,27 @@ function generateRandomStars(maxWidth, maxHeight, amount) {
   return starList;
 }
 
-const starAmount = width > 1000 ? 200 : 100;
-console.log(starAmount);
-
-
 const starContainer = document.querySelector(".star-group-svg");
+const starAmount = width > 1000 ? 200 : 100;
+
 /* Lots of code and inspiration from https://www.satellytes.com/blog/twinkling-night-sky-with-shooting-stars-made-in-svg */
-generateRandomStars(width, height, starAmount).forEach(starCordinates => {
+generateRandomStarLocations(width, height, starAmount).forEach(starCordinates => {
   const starBox = document.createElementNS("http://www.w3.org/2000/svg", "g");
   const star = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   
-  const radius = .5 + Math.random();
+  const brightness = 1 * (Math.random()) + .5 // prevents low light stars like .1
   const delay = 100 +  (Math.random() * 500);
   const duration = 3000 + (Math.random() * 4000);
-  const brightness = 1 * Math.random();
-  
+  const radius = .5 + Math.random();
+
   star.classList.add("star");
   star.setAttribute("r", radius)
   starBox.setAttribute("transform", `translate(${starCordinates.x}, ${starCordinates.y})`);
  
+  star.style.setProperty('--star-brightness', `${brightness}`);
   star.style.setProperty('--star-animation-delay', `${delay}ms`);
   star.style.setProperty('--star-animation-duration', `${duration}ms`);
   star.style.setProperty('--star-animation-glow-duration', `10000ms`);
-  star.style.setProperty('--star-brightness', `${brightness}`);
   
   starBox.appendChild(star);
   starContainer.appendChild(starBox);
